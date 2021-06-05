071e0fece1f9cdd7a66f32c318612fc1ebd849b5
ssohwi <46528239+ssohwi@users.noreply.github.com>
2021-06-05 17:48:05 +0900

    # restart

--- app.js
+++ app.js
@@ -5,13 +5,12 @@ const cookieParser = require('cookie-parser');
 const logger = require('morgan'); // 로그 기록
 const layouts = require('express-ejs-layouts');
 const dotenv = require('dotenv'); // 보안과 편의를 위해 환경변수 파일 관리`
-//git test
 
 dotenv.config(); // .env 파일로부터 환경변수를 읽어서 process.env로 설정
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
 const { sequelize } = require('./models'); // db 연결객체
-const app = express(); //a
+const app = express();
 
 app.set("view engine", "ejs");
 app.set('views', './views');
